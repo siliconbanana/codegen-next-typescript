8dfe6ec1461ba93b844c02122167b16b817208a0
siliconbanana <mingersai@gmail.com>
2026-03-24 16:55:39 +0800

    Add Google Tag Manager to app component
    Integrate Google Tag Manager into the app layout.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,18 @@ import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Link from "next/link";
+import { GoogleTagManager } from '@next/third-parties/google'
+
+export default function AcrossAllPages({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <DataProvider>
+                <Component {...pageProps} />
+            </DataProvider>
+            <GoogleTagManager gtmId="GTM-MPZZC7XZ" />
+        </>
+    );
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
